8a36f196e82f167db6014e067f1eb8889afc8991
vimaljoseph187-arc <vimaljoseph187@gmail.com>
2026-02-26 14:21:56 +0530

    first commit

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
-
+import App from './App.jsx'
 import Dashboard from './pages/dashboard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Dashboard/>
-    {/* <App /> */}
+    {/* <Dashboard/>  */}
+    <App />
   </StrictMode>,
 )
